7c670c513de70e03ad597e012ffb3dd662843188
peaches <peizhe2001@hotmail.com>
2023-12-27 23:38:20 -0500

    Add more images

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,8 @@ export default function MainPage() {
   return (
     <main className="flex py-10">
       <div className="flex flex-col w-full ">
-        <div className="flex flex-col sm:px-20 -mb-56 relative">
-          <div className="absolute sm:w-[32vw] sm:ml-24 aspect-square">
+        <div className="flex flex-col sm:px-20 -mb-56">
+          <div className="sm:w-[770px] sm:place-self-start place-self-center sm:ml-24 aspect-square absolute sm:px-0 px-2">
             <Image
               as={NextImage}
               width={1500}
@@ -16,11 +16,11 @@ export default function MainPage() {
               className="z-0 opacity-100 w-full mix-blend-difference rounded-none object-cover"
             />
           </div>
-          <p className="main-font text-bla text-left 2xl:ml-32 lg:ml-20 ml-0 h-1/3 flex items-center mt-20 text-[100px] sm:text-[150px] lg:text-[250px] 2xl:text-[400px]">
+          <p className="main-font text-bla text-left 2xl:ml-32 lg:ml-20 ml-0 h-1/3 flex items-center mt-20 text-[100px] sm:text-[150px] lg:text-[250px] 2xl:text-[400px] sm:px-0 pl-4">
             PEIZHE
           </p>
           <div className="flex lg:flex-row flex-col-reverse">
-            <div className="2xl:text-4xl lg:text-2xl text-xl text-wh 2xl:ml-32 lg:ml-20 2xl:p-10 p-8 lg:mt-8 h-fit z-0">
+            <div className="2xl:text-4xl lg:text-2xl text-xl text-wh 2xl:ml-24 lg:ml-20 2xl:p-10 p-8 lg:mt-8 h-fit z-0 sm:text-start text-center">
               Computer Engineer Student @ Concordia University
               <div className="flex flex-row gap-20 mt-5 w-full justify-center">
                 <Button
@@ -39,7 +39,7 @@ export default function MainPage() {
                 </Button>
               </div>
             </div>
-            <p className="main-font text-bla text-right 2xl:mr-32 lg:mr-20 mr-0 ml-auto 2xl:-mt-48 lg:-mt-32 sm:-mt-24 -mt-16 flex items-center text-[100px] sm:text-[150px] lg:text-[250px] 2xl:text-[400px] h-fit">
+            <p className="main-font text-bla text-right 2xl:mr-32 lg:mr-20 mr-0 ml-auto 2xl:-mt-48 lg:-mt-32 sm:-mt-24 -mt-16 flex items-center text-[100px] sm:text-[150px] lg:text-[250px] 2xl:text-[400px] h-fit sm:px-0 pr-4">
               TIAN
             </p>
           </div>
