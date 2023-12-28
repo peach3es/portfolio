dfd28e2737a0be2195cc756002d173ec0bd3260a
peaches <peizhe2001@hotmail.com>
2023-12-27 21:31:50 -0500

    Add image and experience section

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,74 @@
+import { Button, Image } from "@nextui-org/react";
+import NextImage from "next/image";
+
 export default function MainPage() {
   return (
-    <main className="flex ">
+    <main className="flex py-10">
       <div className="flex flex-col w-full ">
-        <div className="flex flex-col px-20 -mb-56">
-          <p className="main-font text-bla text-left 2xl:ml-32 lg:ml-20 ml-0 h-1/3 flex items-center mt-20 text-[100px] sm:text-[150px] lg:text-[250px] 2xl:text-[400px] ">
+        <div className="flex flex-col sm:px-20 -mb-56 relative">
+          <div className="absolute sm:w-[32vw] sm:ml-24 aspect-square">
+            <Image
+              as={NextImage}
+              width={1500}
+              height={500}
+              alt="iridescence"
+              src="/img/iridescent.jpg"
+              className="z-0 opacity-100 w-full mix-blend-difference rounded-none object-cover"
+            />
+          </div>
+          <p className="main-font text-bla text-left 2xl:ml-32 lg:ml-20 ml-0 h-1/3 flex items-center mt-20 text-[100px] sm:text-[150px] lg:text-[250px] 2xl:text-[400px]">
             PEIZHE
           </p>
           <div className="flex lg:flex-row flex-col-reverse">
-            <div className="2xl:text-4xl lg:text-2xl text-xl text-bl 2xl:ml-32 lg:ml-20 2xl:p-10 p-8 lg:mt-8 h-fit ">
+            <div className="2xl:text-4xl lg:text-2xl text-xl text-wh 2xl:ml-32 lg:ml-20 2xl:p-10 p-8 lg:mt-8 h-fit z-0">
               Computer Engineer Student @ Concordia University
+              <div className="flex flex-row gap-20 mt-5 w-full justify-center">
+                <Button
+                  disableRipple
+                  className="button w-fit text-2xl main-font font-extrabold bg-transparent text-wh transition ease-in-out duration-300 border-b-1"
+                  radius="none"
+                >
+                  <a href="mailto:peizhe2001@hotmail.com">Contact Me</a>
+                </Button>
+                <Button
+                  disableRipple
+                  className="button w-fit text-2xl main-font font-extrabold bg-transparent text-wh  transition ease-in-out duration-300 border-b-1"
+                  radius="none"
+                >
+                  Resume
+                </Button>
+              </div>
             </div>
             <p className="main-font text-bla text-right 2xl:mr-32 lg:mr-20 mr-0 ml-auto 2xl:-mt-48 lg:-mt-32 sm:-mt-24 -mt-16 flex items-center text-[100px] sm:text-[150px] lg:text-[250px] 2xl:text-[400px] h-fit">
               TIAN
             </p>
           </div>
         </div>
-        {/* ABOUT */}
-        <div className="about marquee text-8xl main-font text-wh">
-          <span>ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT</span>
-          <span>&nbsp;ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT</span>
-          <div className="content h-fit">about section</div>
-        </div>
-        {/* PROJECTS */}
-        <div className="project marquee text-8xl main-font text-wh">
-          <span>
-            PROJECTS PROJECTS PROJECTS PROJECTS PROJECTS PROJECTS&nbsp;
-          </span>
-          <span>PROJECTS PROJECTS PROJECTS PROJECTS PROJECTS PROJECTS</span>
-          <div className="content h-fit">project section</div>
+        <div className="mt-72 sm:mt-0">
+          {/* ABOUT */}
+          <div className="about marquee text-8xl main-font text-wh ">
+            <span>ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT</span>
+            <span>&nbsp;ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT ABOUT</span>
+            <div className="content h-fit mx-10 text-5xl">about section</div>
+          </div>
+          {/* PROJECTS */}
+          <div className="project marquee text-8xl main-font text-wh">
+            <span>
+              PROJECTS PROJECTS PROJECTS PROJECTS PROJECTS PROJECTS&nbsp;
+            </span>
+            <span>PROJECTS PROJECTS PROJECTS PROJECTS PROJECTS PROJECTS</span>
+            <div className="content h-fit mx-10 text-5xl">project section</div>
+          </div>
+          {/* EXPERIENCE */}
+          <div className="experience marquee text-8xl main-font text-wh">
+            <span>
+              EXPERIENCE EXPERIENCE EXPERIENCE EXPERIENCE EXPERIENCE&nbsp;
+            </span>
+            <span>EXPERIENCE EXPERIENCE EXPERIENCE EXPERIENCE EXPERIENCE</span>
+            <div className="content h-fit mx-10 text-5xl">
+              experience section
+            </div>
+          </div>
         </div>
       </div>
     </main>
